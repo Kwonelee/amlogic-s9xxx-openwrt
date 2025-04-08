c9349890d84181d2a52e4d8cb86c2f5304b84087
Kwonelee <995546054@qq.com>
2025-04-08 15:53:11 +0800

    Update diy-part1.sh

--- config/immortalwrt-master/diy-part1.sh
+++ config/immortalwrt-master/diy-part1.sh
@@ -9,6 +9,12 @@
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
+# 修改系统版本（界面显示）
+VERSION=${GITHUB_WORKSPACE}/immortalwrt/version
+VERSION_TEXT=$(head -n 1 ${VERSION} | tr -d ' \r\n')
+if [ -n "$VERSION_TEXT" ]; then
+	sed -i "/^VERSION_NUMBER:=.*SNAPSHOT/s/SNAPSHOT/${VERSION_TEXT}/" include/version.mk
+fi
+
 # other
 # rm -rf package/emortal/{autosamba,ipv6-helper}
-
