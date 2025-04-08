a253b448a6beb6e8787dad677de321213a09e9de
Kwonelee <995546054@qq.com>
2025-04-08 15:53:28 +0800

    Update diy-part2.sh

--- config/immortalwrt-master/diy-part2.sh
+++ config/immortalwrt-master/diy-part2.sh
@@ -38,9 +38,24 @@ git clone https://github.com/tty228/luci-app-wechatpush.git package/luci-app-wec
 # luci-app-zerotier
 rm -rf feeds/luci/applications/luci-app-zerotier
 git clone https://github.com/lwb1978/luci-app-zerotier.git package/luci-app-zerotier
+
+# 添加主题
+rm -rf feeds/luci/themes/luci-theme-argon
+# git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+merge_package openwrt-24.10 https://github.com/sbwml/luci-theme-argon package luci-theme-argon
+# git clone --depth=1 -b js https://github.com/sirpdboy/luci-theme-kucat package/luci-theme-kucat
+
+# TTYD设置
+sed -i 's/procd_set_param stdout 1/procd_set_param stdout 0/g' feeds/packages/utils/ttyd/files/ttyd.init
+sed -i 's/procd_set_param stderr 1/procd_set_param stderr 0/g' feeds/packages/utils/ttyd/files/ttyd.init
+
+# 精简 UPnP 菜单名称
+sed -i 's#\"title\": \"UPnP IGD \& PCP\"#\"title\": \"UPnP\"#g' feeds/luci/applications/luci-app-upnp/root/usr/share/luci/menu.d/luci-app-upnp.json
+# 移动 UPnP 到 “网络” 子菜单
+sed -i 's/services/network/g' feeds/luci/applications/luci-app-upnp/root/usr/share/luci/menu.d/luci-app-upnp.json
+
 #
 # Apply patch
 # git apply ../config/patches/{0001*,0002*}.patch --directory=feeds/luci
 #
 # ------------------------------- Other ends -------------------------------
-
